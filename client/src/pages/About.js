8738cc4f006e2c64442d79d1d5cc6a458e4539cb
nasir hanif <101257760+NASIR-HANIF@users.noreply.github.com>
2023-12-27 02:09:01 +0500

    ok

---
+++ client/src/pages/About.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout title={"About us - Ecommer app"}>
+      <div className="row contactus "  style={{ minHeight: "100vh" }}>
+        <div className="col-md-6 mt-sm-5">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+           className="mx-auto img-fluid"
+           style={{ minHeight: 30 }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
+            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
+            perferendis eius temporibus dicta blanditiis doloremque explicabo
+            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
+            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
+            commodi illum quidem neque tempora nam.
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
---
+++ config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import colors from "colors";
+import  dotenv  from 'dotenv';
+dotenv.config()
+
+
+const connectDB = async () => {
+const  DB_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@myfirstcluster.2zedvpv.mongodb.net/ecommerce`
+// const DB_URL = 'mongodb://127.0.0.1:27017/ecommerce'
+  try {
+    const conn = await mongoose.connect(DB_URL);
+    console.log(
+      `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
+    );
+  } catch (error) {
+    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+  }
+};
+
+export default connectDB;
